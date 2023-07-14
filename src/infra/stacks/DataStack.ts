ec668715f457ad4d8f144bf2aaa0193df3f8f708
hugo cm <hugocmdesign@gmail.com>
2023-07-15 00:58:58 +0200

    create and export value of DynamoDB

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,8 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import { getSuffixFromStack } from '../Utils';
 
 export class DataStack extends Stack {
+  public readonly spaceTalbe: ITable;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const suffix = getSuffixFromStack(this);
+
+    this.spaceTalbe = new Table(this, 'SpacesTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      tableName: `SpacesTable-${suffix}`,
+    });
   }
 }
