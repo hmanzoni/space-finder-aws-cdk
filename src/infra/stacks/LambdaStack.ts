8c3711c3440feee5c7a0e1ae18c8af5944ff277f
hugo cm <hugocmdesign@gmail.com>
2023-07-15 00:41:13 +0200

    Add ApiStack and export param from LambdaStack

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,16 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function as LambdaFn, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { join } from 'path';
 
 export class LambdaStack extends Stack {
+  public readonly helloLambdaIntegration: LambdaIntegration;
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new LambdaFn(this, 'HelloLambda', {
+    const helloLambda = new LambdaFn(this, 'HelloLambda', {
       runtime: Runtime.NODEJS_18_X,
       handler: 'hello.mail',
       code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
     });
+    this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
   }
 }
