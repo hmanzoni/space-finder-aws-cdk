fdddac5037e83268a7c5fe3f5cb9fa49fee64a2a
hugo cm <hugocmdesign@gmail.com>
2023-07-15 22:01:30 +0200

    Add the correct policy for allow the call to s3

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,7 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
-import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path';
@@ -23,6 +24,12 @@ export class LambdaStack extends Stack {
         TABLE_NAME: props.spacesTable.tableName,
       },
     });
+    const policyAllowS3Interaction = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ['s3:ListAllMyBuckets', 's3:ListBucket'],
+      resources: ['*'],
+    });
+    helloLambda.addToRolePolicy(policyAllowS3Interaction);
     this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
   }
 }
