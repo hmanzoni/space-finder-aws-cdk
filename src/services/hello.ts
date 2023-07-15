c692bedad472045d3ed1ed8e015d23548fcf0aca
hugo cm <hugocmdesign@gmail.com>
2023-07-15 22:01:05 +0200

    Add a call to s3 on my lambda code

--- src/services/hello.ts
+++ src/services/hello.ts
@@ -3,12 +3,20 @@ import {
   APIGatewayProxyResult,
   Context,
 } from 'aws-lambda';
+import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
+
+const s3Client = new S3Client({});
 
 async function hanblder(event: APIGatewayProxyEvent, context: Context) {
+  const command = new ListBucketsCommand({});
+  const listBucketsResult = (await s3Client.send(command)).Buckets;
+
   const resp: APIGatewayProxyResult = {
     statusCode: 200,
     body: JSON.stringify(
-      `Hello! I will read from Dynamo Table > ${process.env.TABLE_NAME} ! :D`
+      `Hello! I will read from Dynamo Table > ${
+        process.env.TABLE_NAME
+      } ! :D, my S3 Buckets are ${JSON.stringify(listBucketsResult)}`
     ),
   };
   console.log(event);
