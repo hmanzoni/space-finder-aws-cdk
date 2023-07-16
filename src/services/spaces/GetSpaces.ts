05cea1e5f53061fd3c1f6a6c10d9bcb2d7244d72
hugo cm <hugocmdesign@gmail.com>
2023-07-16 23:31:09 +0200

    add IDs option on get and handle the resp with util-ddb

--- src/services/spaces/GetSpaces.ts
+++ src/services/spaces/GetSpaces.ts
@@ -1,20 +1,58 @@
-import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  ScanCommand,
+} from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 async function getSpaces(
   event: APIGatewayProxyEvent,
   ddbClient: DynamoDBClient
 ): Promise<APIGatewayProxyResult> {
+  if (event.queryStringParameters) {
+    if ('id' in event.queryStringParameters) {
+      const spaceId = event.queryStringParameters['id'];
+      const getItemResp = await ddbClient.send(
+        new GetItemCommand({
+          TableName: process.env.TABLE_NAME,
+          Key: {
+            id: {
+              S: spaceId,
+            },
+          },
+        })
+      );
+      if (getItemResp.Item) {
+        const unmashalledItem = unmarshall(getItemResp.Item);
+        return {
+          statusCode: 200,
+          body: JSON.stringify(unmashalledItem),
+        };
+      } else {
+        return {
+          statusCode: 401,
+          body: JSON.stringify(`Space with id ${spaceId} not found!`),
+        };
+      }
+    } else {
+      return {
+        statusCode: 401,
+        body: JSON.stringify('Id required!'),
+      };
+    }
+  }
   const result = await ddbClient.send(
     new ScanCommand({
       TableName: process.env.TABLE_NAME,
     })
   );
-  console.log(result.Items);
+  const unmashalledItems = result.Items?.map((item) => unmarshall(item));
+  console.log(unmashalledItems);
 
   return {
     statusCode: 201,
-    body: JSON.stringify(result.Items),
+    body: JSON.stringify(unmashalledItems),
   };
 }
 
