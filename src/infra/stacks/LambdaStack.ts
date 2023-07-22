b878ec9ae2d8d4fba1201e98082fccd20e19cc09
hugo cm <hugocmdesign@gmail.com>
2023-07-22 18:15:09 +0200

    Activate the x-ray for the lambda

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -2,7 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path';
@@ -23,6 +23,7 @@ export class LambdaStack extends Stack {
       environment: {
         TABLE_NAME: props.spacesTable.tableName,
       },
+      tracing: Tracing.ACTIVE,
     });
 
     const policyAllowDDBInteraction = new PolicyStatement({
