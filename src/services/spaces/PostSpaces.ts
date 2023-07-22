72ffcdef9f5795b7230be8f500233becc2687ee3
hugo cm <hugocmdesign@gmail.com>
2023-07-22 15:06:35 +0200

    change method using marshall from sdk

--- src/services/spaces/PostSpaces.ts
+++ src/services/spaces/PostSpaces.ts
@@ -1,27 +1,24 @@
 import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { validateAsSpaceEntry } from '../shared/Validator';
+import { marshall } from '@aws-sdk/util-dynamodb';
 import { createRandomId, parseJSON } from '../shared/Utils';
 
 async function postSpaces(
   event: APIGatewayProxyEvent,
   ddbClient: DynamoDBClient
 ): Promise<APIGatewayProxyResult> {
-  const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
   const randomId = createRandomId();
   const item = parseJSON(event.body);
   item.id = randomId;
   validateAsSpaceEntry(item);
 
-  const result = await ddbDocClient.send(
+  const result = await ddbClient.send(
     new PutItemCommand({
       TableName: process.env.TABLE_NAME,
-      // Item: { id: { S: randomId }, location: { S: item.location } },
-      Item: item,
+      Item: marshall(item),
     })
   );
-  console.log(result);
 
   return {
     statusCode: 201,
