f82f6652651cb058a01ab7cdeb89fa96c372167d
hugo cm <hugocmdesign@gmail.com>
2023-07-22 18:12:19 +0200

    Add monitoring, 4xx alarm to the API

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -3,6 +3,7 @@ import { DataStack } from './stacks/DataStack';
 import { LambdaStack } from './stacks/LambdaStack';
 import { ApiStack } from './stacks/ApiStack';
 import { AuthStack } from './stacks/AuthStack';
+import { MonitorStack } from './stacks/MonitorStack';
 
 const app = new App();
 const dataStack = new DataStack(app, 'DataStack');
@@ -14,3 +15,4 @@ new ApiStack(app, 'ApiStack', {
   lambdaIntegration: lambdaStack.lambdaIntegration,
   userPool: authStack.userPool,
 });
+new MonitorStack(app, 'MonitorStack');
